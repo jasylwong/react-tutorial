5c43a4b9645348b5c16a0cb08418562082aab111
Jason <jasylwong@gmail.com>
2020-04-18 19:29:17 +0100

    change background by button flick;

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import Room from './Room'
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      {/* <MediaCard title="Media Card" body="Media card content" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/womanyellingcat-1573233850.jpg"/> */}
-      {/* <Gate isOpen = {true}/> */}
-      <Room />
-    </div>
-  );
+  return <Room />
+    // <div>
+    //   {/* <MediaCard title="Media Card" body="Media card content" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/womanyellingcat-1573233850.jpg"/> */}
+    //   {/* <Gate isOpen = {true}/> */}
+
+    // </div>
 }
 
 export default App;
--- src/Room.js
+++ src/Room.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 const Room = function() {
   const [isLit, setLit] = useState(true);
+  const brightness = isLit? 'lit' : 'dark'
   return (
-    <div>
-      <div className="room">The room is {isLit ? 'lit' : 'dark'}</div>
+    <div className={`room ${brightness}`}>
+      The room is { isLit? 'lit' : 'dark' }
+      <br />
+      <button onClick={ () => setLit(!isLit) } >
+        flick
+      </button>
     </div>
   )
 }
