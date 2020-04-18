4eb9ad8ead875cad3f3e70d4fc5235936f3900e8
Jason <jasylwong@gmail.com>
2020-04-18 20:11:20 +0100

    add temp up and down buttons

--- src/Room.js
+++ src/Room.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 const Room = function() {
   const [isLit, setLit] = useState(true);
   const brightness = isLit? 'lit' : 'dark'
+  const [temp, setTemp] = useState(22);
   return (
     <div className={`room ${brightness}`}>
       The room is { isLit? 'lit' : 'dark' }
       <br />
+      
       <button onClick={ () => setLit(!isLit) } >
         flick
       </button>
@@ -15,6 +17,15 @@ const Room = function() {
       </button>
       <button onClick={ () => setLit(false)} >
         Off
+      </button>
+      <br />
+      The temperature is { temp } degrees celsius.
+      <br />
+      <button onClick={ () => setTemp(temp+1) } >
+        +
+      </button>
+      <button onClick={ () => setTemp(temp-1) }>
+        -
       </button>
     </div>
   )
