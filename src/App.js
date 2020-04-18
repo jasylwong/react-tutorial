b3eae1dfa0957507dd97921f09051af1fb5ee88b
Jason <jasylwong@gmail.com>
2020-04-18 18:24:59 +0100

    import Gate to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MediaCard from './MediaCard'
+import Gate from './Gate'
 import './App.css';
 
 function App() {
   return (
     <div>
       <MediaCard title="Media Card" body="Media card content" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/womanyellingcat-1573233850.jpg"/>
+      <Gate />
     </div>
   );
 }
