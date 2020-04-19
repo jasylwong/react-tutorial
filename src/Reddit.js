1e30010dd48cbf87871e5c90086fe27eedc83d14
Jason <jasylwong@gmail.com>
2020-04-19 13:09:40 +0100

    make title a hyperlink to actual page for each post

--- src/Reddit.js
+++ src/Reddit.js
@@ -16,7 +16,7 @@ function Reddit() {
       <h2>Reddit posts</h2>
       <ul>
         {posts.map(post => {
-          return <li key={post.id}>{post.title} , {post.score}, {post.author_fullname}</li>
+          return <li key={post.id}><a href={post.url}>{post.title}</a>, {post.score}, {post.author_fullname}</li>
         })}
       </ul>
     </div>
