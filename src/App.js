53d9205f910d281ecd1ebdfc41d5005b273b31a4
Jason <jasylwong@gmail.com>
2020-04-19 12:09:01 +0100

    create Reddit component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import MediaCard from './MediaCard'
 import Gate from './Gate'
 import Room from './Room'
+import Reddit from './Reddit'
 import './App.css';
 
 function App() {
-  return <Room />
+  return <Reddit />
     // <div>
     //   {/* <MediaCard title="Media Card" body="Media card content" imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/womanyellingcat-1573233850.jpg"/> */}
     //   {/* <Gate isOpen = {true}/> */}
